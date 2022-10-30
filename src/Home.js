62b17dd2f0c5dab75c7a8d6914b99c0bb7bd858f
HIDRI A <agthidri24@yahoo.com>
2022-10-30 10:31:44 -0500

    this added useState

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,21 @@
+import {useState} from 'react';
+
 const Home = () => {
+
+    // let name ='mario';
+    const [name, setName] =useState('mario');
+    const[age , setAge] = useState(25)
+
+    const handleClick = () => {
+       setName('hidri');
+       setAge(34);
+    }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <h4>{name} is  {age} old </h4>
+            <button onClick={handleClick}>Click me</button>
         </div>
     )
 
